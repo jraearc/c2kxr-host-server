609165c8c68017904b87fadf6d606d6aca7afbab
Jahziel Rae Arceo <rae.arceo@live.com>
2018-02-03 11:35:13 +0800

    Add more API calls

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/api/events', 'EventController@getAllEvents');
 
 Route::post('/api/events/add', 'EventController@addEvent');
 
-Route::post('/api/events/rm', 'EventController@removeEvent');
+Route::post('/api/events/rm/{owner_id}', 'EventController@removeEvent');
 
 Route::get('/api/events/{id}', 'EventController@getByEventOwner');
